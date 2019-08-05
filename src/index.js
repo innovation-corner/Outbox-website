98ebaec818ed28032cd3f8e19c1d5c4cf35717ee
destiny.ajakaiye-at-968906387115 <destinyajakaiye@gmail.com>
2019-08-05 17:06:34 +0000

    Added background slider

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import Home from './pages/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <Home />, 
     document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,40 +9,50 @@ import {
     Button
 } from 'reactstrap';
 import TopNavBar from '../components/NavBar/index';
+import BackgroundSlideshow from 'react-background-slideshow';
+
+import image1 from '../assets/images/background-1.jpeg';
+import image2 from '../assets/images/background-2.jpeg';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                 <TopNavBar />
-                <Container className="main-body">
-                    <Row>
-                        <Col xs="6" sm="3" className="form-box">
-                            <div className="form-header">
-                                <h3>Sale your space</h3>
-                                <p>Or select a folder</p>
-                            </div>
-                            <Form className="form-content">
-                                <FormGroup>
-                                    <Input type="email" name="email" placeholder="Email to" />
-                                </FormGroup>
-                                <FormGroup>
-                                    <Input type="email" name="email" placeholder="Your email" />
-                                </FormGroup>
-                                <FormGroup>
-                                    <Input type="textarea" name="text" id="exampleText" placeholder="Message"/>
-                                </FormGroup>
-                            </Form>
-                            <div className="form-footer">
-                                <FormGroup>
-                                    <Button color="primary" disabled>Transfer</Button>
-                                </FormGroup>
-                            </div>
-                        </Col>
-                        <Col xs="6" sm="3"></Col>
-                        <Col xs="6" sm="4"></Col>
-                    </Row>
-                </Container>
+                <div className="content-container">
+                    <TopNavBar />
+                    <Container className="main-body" fluid>
+                        <Row className="first-row">
+                            <Col xs="12" sm="2" className="form-box">
+                                <div className="form-header">
+                                    <h3>Sale your space</h3>
+                                    <p>Or select a folder</p>
+                                </div>
+                                <Form className="form-content">
+                                    <FormGroup>
+                                        <Input type="email" name="email" placeholder="Email to" />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Input type="email" name="email" placeholder="Your email" />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Input type="textarea" name="text" id="exampleText" placeholder="Message"/>
+                                    </FormGroup>
+                                </Form>
+                                <div className="form-footer">
+                                    <FormGroup>
+                                        <Button color="primary" disabled>Transfer</Button>
+                                    </FormGroup>
+                                </div>
+                            </Col>
+                            <Col xs="12" sm="3"></Col>
+                            <Col xs="12" sm="4"></Col>
+                        </Row>
+                    </Container>
+                </div>
+
+                <div className="background-container">
+                    <BackgroundSlideshow images={[ image1, image2 ]} />
+                </div>
             </div>
         );
     };
