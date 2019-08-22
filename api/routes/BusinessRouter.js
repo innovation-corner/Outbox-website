ef97b68e34e7b1f8e28366da3297e690dc60ce09
Chinedu Ebinim <chinedu@verifi.ng>
2019-08-22 07:36:26 +0100

    edit route

--- api/routes/BusinessRouter.js
+++ api/routes/BusinessRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const business = require("../Controllers/BusinessController");
 
-router.put("/:id", business.edit);
+router.put("/edit/:id", business.edit);
 router.get("/:id/users", business.listUsers);
 
 module.exports = router;
