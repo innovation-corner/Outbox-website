1b6b15173fa570ecafc09f68ae1f5b9e7689c174
Chinedu Ebinim <chinedu@verifi.ng>
2019-08-29 14:30:50 +0100

    fix error

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,13 +3,14 @@ const authRouter = require("./AuthRouter");
 const businessRouter = require("./BusinessRouter");
 const locationRouter = require("./LocationRouter");
 const bookingRouter = require("./BookingRouter");
-const businessRouter = require("./BusinessRouter");
 const roomRouter = require("./RoomRouter");
 
 module.exports = {
   userRouter,
   locationRouter,
   // deskRouter,
+  roomRouter,
   businessRouter,
+  bookingRouter,
   authRouter
 };
