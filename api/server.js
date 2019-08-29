5ec508924e2d057e58d5f717c2338577d9bcf2ef
Chinedu Ebinim <chinedu@verifi.ng>
2019-08-29 13:56:02 +0100

    fix merge issues

--- api/server.js
+++ api/server.js
@@ -17,16 +17,11 @@ const router = require("./routes");
 
 app.use("/api/v1/auth", router.authRouter);
 app.use("/api/v1/user",passport.authenticate('jwt', { session: false }), router.userRouter);
-app.use("/api/v1/booking",passport.authenticate('jwt', { session: false }), router.bookingrouter);
+app.use("/api/v1/book",passport.authenticate('jwt', { session: false }), router.bookingRouter);
 app.use("/api/v1/business",passport.authenticate('jwt', { session: false }), router.businessRouter);
 app.use("/api/v1/location",passport.authenticate('jwt', { session: false }), router.locationRouter);
-<<<<<<< HEAD
-app.use("/api/v1/business",passport.authenticate('jwt', { session: false }), router.businessRouter);
-app.use("/api/v1/book",passport.authenticate('jwt', { session: false }), router.bookingRouter);
 app.use("/api/v1/room",passport.authenticate('jwt', { session: false }), router.roomRouter);
 
-=======
->>>>>>> bcda7d5c2ea75e4d04ce1d5c0923843024e6b0be
 
 /** starting up the server */
 app.listen(port, () => {
