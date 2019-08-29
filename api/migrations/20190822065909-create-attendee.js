94008fcfd6c96701df078fe85d06ace49eaf780c
Chinedu Ebinim <chinedu@verifi.ng>
2019-08-29 08:53:04 +0100

    add endTime

--- api/migrations/20190822065909-create-attendee.js
+++ api/migrations/20190822065909-create-attendee.js
@@ -20,6 +20,9 @@ module.exports = {
         type: Sequelize.DATE
       },
       duration: Sequelize.INTEGER,
+      endTime: {
+        type: Sequelize.DATE
+      },
       bookingId: {
         type: Sequelize.INTEGER,
         references: {
--- api/models/attendee.js
+++ api/models/attendee.js
@@ -7,6 +7,9 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.DATE
       },
       duration: DataTypes.INTEGER,
+      endTime: {
+        type: DataTypes.DATE
+      }
     },
     {}
   );
