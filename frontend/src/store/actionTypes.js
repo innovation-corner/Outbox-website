dd32ccd3981911c070e670e096631794608b966b
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2019-08-31 12:43:08 +0100

    Extended redux settings

---
+++ frontend/src/store/actionTypes.js
@@ -0,0 +1,5 @@
+export const SIGNIN = 'SIGNIN';
+export const SIGNUP = 'SIGNUP';
+export const SIGNOUT = 'SIGNOUT';
+export const SIGNIN_SUCCESS = 'SIGNIN_SUCCESS';
+export const SIGNIN_FAILURE = 'SIGNIN_FAILURE';
