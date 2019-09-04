d081ec2e25e3978590a98ab6af720985b53cb728
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2019-09-04 06:17:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -20,17 +20,17 @@ app.use(bodyParser.json());
 
 const router = require("./routes");
 
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
+
 app.use("/api/v1/auth", router.authRouter);
 app.use("/api/v1/user",passport.authenticate('jwt', { session: false }), router.userRouter);
 app.use("/api/v1/book",passport.authenticate('jwt', { session: false }), router.bookingRouter);
 app.use("/api/v1/business",passport.authenticate('jwt', { session: false }), router.businessRouter);
 app.use("/api/v1/location",passport.authenticate('jwt', { session: false }), router.locationRouter);
 app.use("/api/v1/room",passport.authenticate('jwt', { session: false }), router.roomRouter);
-
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
-});
 
 /** starting up the server */
 app.listen(port, () => {
